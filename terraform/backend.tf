2d70b48a18ba3c6d29c19c36edff9e4716cf963d
Monaliza Vitor <vitormonaliza@gmail.com>
2025-10-10 21:56:01 -0300

    fix terraform backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
     endpoint                    = "https://nyc3.digitaloceanspaces.com"
     region                      = "us-east-1" # Required but not used by DO Spaces
     bucket                      = "giropops-terraform-state"
