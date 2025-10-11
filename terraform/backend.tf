8fafde074cb0334ad3ebda06f76bf296a93b0fcf
Monaliza Vitor <vitormonaliza@gmail.com>
2025-10-10 22:10:46 -0300

    fix: terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,9 +3,11 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    endpoint                    = "https://nyc3.digitaloceanspaces.com"
-    region                      = "us-east-1" # Required but not used by DO Spaces
-    bucket                      = "giropops-terraform-state"
-    key                         = "kubernetes/terraform.tfstate"
+    endpoints = {
+      s3 = "https://nyc3.digitaloceanspaces.com"
+    }
+    region = "us-east-1" # Required but not used by DO Spaces
+    bucket = "giropops-terraform-state"
+    key    = "kubernetes/terraform.tfstate"
   }
 }
